47d8968b1d1c86d1fc9bf4e74616f20fac8a97fa
mshinno26 <mshinno26@punahou.edu>
2025-07-12 14:52:31 -1000

    7/12/2025 Transceivers + comments
    Added comments for documentation purposes
    
    Might edit transceivers so that, instead of ALWAYS sending button state, only send when button is pressed; harder to debug if something stops working but easier on hardware

--- src/ReceiverWorking/ReceiverWorking.ino
+++ src/ReceiverWorking/ReceiverWorking.ino
@@ -1,45 +1,58 @@
-// all libraries
+// serial, radio, and servo libraries
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Servo.h>
 
+// transceiver pins, radio constructor
 int csn_pin = 8;
 int ce_pin = 7;
 RF24 radio(ce_pin, csn_pin);
 
+// will store output received
 int botton;
+// stores previous button state to prevent issues with holding button
 int bottonState = false;
 
+// channel matches with transmitter
 const byte address[6] = "00001";
 
+// servo constructor, servo pins
 Servo myservo;
 int servoPin = 5;
 int open = 127;
 int closed = 37;
 
 void setup() {
+  // baud rate 9600
   Serial.begin(9600);
+
+  // set up radio; address 00001, pipe 0, minimum power level, set as receiver
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 
+  // set servo pin, move to default position
   myservo.attach(servoPin);
   myservo.write(closed);
 }
 
 void loop() {
+  // check for unread transmission
   if (radio.available()) {
-    // char text[32] = "";
+    // read transmission into "botton" int
     radio.read(&botton, sizeof(botton));
     Serial.println(botton);
 
+    // when button pressed and it wasn't pressed before
     if (botton && !bottonState) {
+      // open, delay .5 seconds, close
       bottonState = true;
       myservo.write(open);
       delay(500);
       myservo.write(closed);
+      // second delay to allow servo to move before receiving again
       delay(500);
     } else if (!botton && bottonState) {
       bottonState = false;
--- src/TransmitterWorking/TransmitterWorking.ino
+++ src/TransmitterWorking/TransmitterWorking.ino
@@ -1,23 +1,31 @@
+// include serial and radio libraries
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
+// radio pins and constructor
 int ce_pin = 7;
 int csn_pin = 8;
 RF24 radio(ce_pin, csn_pin);
-bool success;
-// int count = 0;
 
+// to store output of RF24.write(): true if received acknowledgement of transmisison, false otherwise
+bool success;
+
+// button pin, light pin; var to store output of digitalRead() of button
 int buttonPin = 3;
 int ledPIN = 4;
 int buttonState;
 
+// address matches receiver
 const byte address[6] = "00001";
 
 void setup() {
+  // baud rate 9600
   Serial.begin(9600);
+  // set button pin input and led pin output; transmitter pins handled automatically by library
   pinMode(buttonPin, INPUT);
   pinMode(ledPIN, OUTPUT);
+  // set up radio; address 00001, automatically writes to pipe 0, power level minimum, set as transmitter
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
@@ -25,17 +33,19 @@ void setup() {
 }
 
 void loop() {
-  // count++;
+  // read button input pin; HIGH if pressed, LOW otherwise
   buttonState = digitalRead(buttonPin);
   Serial.print("button state: ");
   Serial.println(buttonState);
+  // send value of "buttonState" int; set bool "success" to true if receive acknowledgement, false otherwise
   success = radio.write(&buttonState, sizeof(buttonState));
+  // if received acknowledgement, light up LED, otherwise turn off
   if (success) {
     digitalWrite(ledPIN, HIGH);
   } else {
     digitalWrite(ledPIN, LOW);
   }
   Serial.println(success);
-  // const char text[] = "Hello World";
+  // wait 0.1 seconds to allow transceivers time to communicate & prevent overloading board
   delay(100);
 }
